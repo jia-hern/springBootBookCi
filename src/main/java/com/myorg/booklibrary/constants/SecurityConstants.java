a51602d30aa1f44dfefc7e71ba1130a0d3314bf0
jh <hojiahern@gmail.com>
2023-08-04 17:49:20 +0800

    add commented 5s for testing

--- src/main/java/com/myorg/booklibrary/constants/SecurityConstants.java
+++ src/main/java/com/myorg/booklibrary/constants/SecurityConstants.java
@@ -3,6 +3,8 @@ package com.myorg.booklibrary.constants;
 public class SecurityConstants {
     public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)";
     public static final int TOKEN_EXPIRATION = 72_00_000; // 2hr in ms
+    // public static final int TOKEN_EXPIRATION = 5_000;
+    // 5s in ms to test auto logout
     public static final String BEARER = "Bearer ";
     public static final String AUTHORIZATION = "Authorization";
     public static final String ROLE_PREFIX = "ROLE_";
